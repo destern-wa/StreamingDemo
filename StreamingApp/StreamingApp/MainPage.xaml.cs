42a008af861120a21ed571f3660025422d34eeaf
David Stern <20023188@tafe.wa.edu.au>
2021-08-13 08:25:25 +0800

    Display and update current track info from api

--- StreamingApp/StreamingApp/MainPage.xaml.cs
+++ StreamingApp/StreamingApp/MainPage.xaml.cs
@@ -11,15 +11,41 @@ namespace StreamingApp
     public partial class MainPage : ContentPage
     {
         bool IsPlaying;
+        string coverImageUri;
+        Track trackInfo;
+
         public MainPage()
         {
             InitializeComponent();
             IsPlaying = false;
+            trackInfo = new Track();
             StatusLabel.Text = "Not yet playing";
         }
         private void UpdateUI()
         {
             StatusLabel.Text = IsPlaying ? "Now playing" : "Stopped playing";
+            ArtistLabel.Text = trackInfo.Artist;
+            SongTitleLabel.Text = trackInfo.Title;
+            if (coverImageUri != trackInfo.ImageUri)
+            {
+                coverImageUri = trackInfo.ImageUri;
+                CoverImage.Source = ImageSource.FromUri(new Uri(coverImageUri));
+            }
+        }
+        private async void UpdateTrackInfo()
+        {
+            await trackInfo.Update();
+            Device.BeginInvokeOnMainThread(() => UpdateUI()); // The UI can only be updated from the main thread
+        }
+
+        private void KeepTrackInfoUpdated()
+        {
+            UpdateTrackInfo();
+            Device.StartTimer(TimeSpan.FromSeconds(15), () =>
+            {
+                UpdateTrackInfo();
+                return IsPlaying;
+            });
         }
 
         private void PlayButton_Clicked(object sender, EventArgs e)
@@ -27,6 +53,7 @@ namespace StreamingApp
             DependencyService.Get<IStreaming>().Play();
             IsPlaying = true;
             UpdateUI();
+            KeepTrackInfoUpdated();
         }
 
 
---
+++ StreamingApp/StreamingApp/Track.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingApp
+{
+    public class Track
+    {
+        public string Artist { get; private set; } = "BOOM Radio";
+        public string Title { get; private set; } = "Not Just Noise";
+        public string ImageUri { get; set; } = "https://cdn-radiotime-logos.tunein.com/s195836q.png";
+
+        private string url = "https://feed.tunein.com/profiles/s195836/nowPlaying";
+        private readonly HttpClient client = new HttpClient();
+
+        private async Task<string> Fetch()
+        {
+            // Fetch data from server's api
+            var response = await client.GetAsync(url);
+
+            // Check for errors
+            if (response.StatusCode != System.Net.HttpStatusCode.OK || response.Content == null)
+            {
+                throw new Exception(string.Format("Data could not be retrieved from the server (code: {0})", response.StatusCode));
+            }
+
+            // Extract the response
+            string responseString = await response.Content.ReadAsStringAsync();
+            return responseString;
+        }
+
+        private void Parse(string responseString)
+        {
+            try
+            {
+                // Parse relevant data out of the json response string
+                JObject response = JsonConvert.DeserializeObject<JObject>(responseString);
+                JObject track = response.Value<JObject>("Secondary");
+                if (track == null)
+                {
+                    track = response.Value<JObject>("Primary");
+                }
+                string info = track.Value<string>("Title");
+                if (info.Contains(" - "))
+                {
+                    string[] parts = info.Split(new string[] { " - " }, StringSplitOptions.None);
+                    Artist = parts[0];
+                    Title = parts[1] == parts[0] ? "" : parts[1];
+                }
+                else
+                {
+                    Artist = info;
+                    Title = track.Value<string>("Subtitle");
+                }
+                ImageUri = track.Value<string>("Image").Replace("http://", "https://");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[Parse error] " + ex.Message);
+                Artist = "BOOM Radio";
+                Title = "Not Just Noise";
+                ImageUri = "https://cdn-radiotime-logos.tunein.com/s195836q.png";
+            }
+        }
+
+        public async Task Update()
+        {
+            string response = await Fetch();
+            Parse(response);
+        }
+    }
+}
